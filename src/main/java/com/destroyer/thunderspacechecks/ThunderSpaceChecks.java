1847b37f8607d2ab861edb3efba65350ca3c567d
Ilya-2025 <kusilla16@gmail.com>
2025-02-25 09:26:32 +0200

    Initial commit

---
+++ src/main/java/com/destroyer/thunderspacechecks/ThunderSpaceChecks.java
@@ -0,0 +1,17 @@
+package com.destroyer.thunderspacechecks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThunderSpaceChecks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
